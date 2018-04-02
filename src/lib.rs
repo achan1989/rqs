4c7686d28e7b1f5ddebc3181d0f7ac09ddb2991f
achan1989 <achan1989@gmail.com>
2018-04-02 18:51:04 +0100

    Initial handling of pack files.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
+extern crate byteorder;
+#[macro_use]
+extern crate failure;
+#[macro_use]
+extern crate failure_derive;
+
 pub mod defs;
 pub mod parms;
 pub use parms::Parms;
+pub mod fs;
 pub mod try_from_temp;
